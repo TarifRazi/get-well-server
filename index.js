eff28f49b0ce9f44356b2b1af8f14a16b83d4e32
Tarif <tarifuddinrajee@gmail.com>
2023-12-11 00:06:50 +0600

    user get and post function added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,13 +30,109 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
+    const userCollection = client.db("get-well").collection("users");
+    const servicesCollection = client.db("get-well").collection("services");
+    const doctorsCollection = client.db("get-well").collection("doctors");
+    const appointmentCollection = client.db("get-well").collection("appointment");
+    const testCollection = client.db("get-well").collection("tests");
+
+
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // get specific id data
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.get('/doctors', async (req, res) => {
+      const cursor = doctorsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+
+    // appointment collection
+
+    app.get('/appointment', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await appointmentCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.delete('/appointment/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await appointmentCollection.deleteMany(query)
+      res.send(result)
+    })
+
+    app.post('/appointment', async (req, res) => {
+      const appointmentDoctor = req.body
+      const result = await appointmentCollection.insertOne(appointmentDoctor)
+      res.send(result)
+    })
+
+    // tests collections
+
+    app.get('/tests', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await testCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/tests/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await testCollection.deleteMany(query)
+      res.send(result)
+    })
+
+    app.post('/tests', async (req, res) => {
+      const bookedTests = req.body
+      const result = await testCollection.insertOne(bookedTests);
+      res.send(result)
+    })
+
+
+    // user related api
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      // check email already exist or not. (need this for social login/registration)
+      const query = {email:user.email}
+      const existUser = await userCollection.findOne(query)
+      if (existUser) {
+        return res.send({message:'user already registered', insertedId:null})
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -44,10 +140,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('get well server is running')
+app.get('/', (req, res) => {
+  res.send('get well server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`get well running on port ${port}`)
+app.listen(port, () => {
+  console.log(`get well running on port ${port}`)
 })
